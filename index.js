55d760c98db3fd6104a137ec71492ae3cc7646db
jjir <jjir@jjirs-MacBook-Pro.local>
2018-04-11 15:30:21 -0500

    Change greeting

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'Gali and Cito'});
+    res.send({hi: 'Inge and Gali and Cito'});
 });
 //Set port variable with Heroku
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 
-
-console.log("Thanks");
+console.log("Server Running at 5000 Porthe");
