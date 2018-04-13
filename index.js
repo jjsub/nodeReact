d5c68be590637bd7332f210b16bf8c69282edda1
jjir <jjir@jjirs-mbp.attlocal.net>
2018-04-12 21:33:07 -0500

    404 error

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-        hi: 'Inge and Gali and Cito',
-        hola: 'Saludos prueba'
-    });
-});
-//Set port variable with Heroku
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
-
-console.log("Server Running at 5000 Porthe");
