3dfee4112089bdf6dff6a14388c09f0ec88c2e3e
jjsub <jsamesub@gmail.com>
2018-04-13 16:05:37 -0500

    Refactor folders and files code

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+// const passportConfig = require('./services/passport');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('TO',accessToken);
-      console.log('TO 2',refreshToken);
-      console.log('TO 3',profile);
-      console.log('TO 4',done);
-    }
-  )
-);
-// https://accounts.google.com/o/oauth2/v2/auth?
-// response_type=code&
-// redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
-// scope=profile%20email&client_id=100492887800-5jg48pei41mof6uppih3jnhkcds2omks.apps.googleusercontent.com
-
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+//is the same as // const authRoutes = require('./routes/authRoutes');
+// authRoutes(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
+app.get('/jjir',(req, res) => {res.send('Hola Mundo!')});
 console.log("Connect @ Port 5000")
