87747f072d3c006b207c082bad97a3b0b55ab840
jjir <>
2018-04-13 15:10:31 -0500

    Setting Aouth token

--- index.js
+++ index.js
@@ -13,11 +13,19 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('TO',accessToken);
+      console.log('TO 2',refreshToken);
+      console.log('TO 3',profile);
+      console.log('TO 4',done);
     }
   )
 );
+// https://accounts.google.com/o/oauth2/v2/auth?
+// response_type=code&
+// redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
+// scope=profile%20email&client_id=100492887800-5jg48pei41mof6uppih3jnhkcds2omks.apps.googleusercontent.com
+
 
 app.get(
   '/auth/google',
@@ -25,6 +33,8 @@ app.get(
     scope: ['profile', 'email']
   })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
