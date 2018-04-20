c559e4098c7872ea6aa1cd862fa67316a42fed64
jjsub <jsamesub@gmail.com>
2018-04-20 17:27:44 -0500

    Auth sign in

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys')
 // const passportConfig = require('./services/passport');
 require('./models/Users')
@@ -10,6 +12,17 @@ require('./services/passport'); // Require will invocate the code in this file
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    //30 days in miliseconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys : [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 //is the same as // const authRoutes = require('./routes/authRoutes');
--- services/passport.js
+++ services/passport.js
@@ -7,6 +7,17 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    //user.id is the MongoDB record id
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
       {
